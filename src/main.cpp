951f30dc9e81012dad8331e10455437afd51a145
desertedman <jonathanjho03@gmail.com>
2025-03-19 10:37:47 -0700

    Initial window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include "Chip8.h"
 
+#include <SDL2/SDL.h>
 #include <iostream>
+
+const int BASE_HEIGHT = 32;
+const int BASE_WIDTH = 64;
+
+const int SCREEN_MULITPLIER = 8;
+
+const int SCREEN_HEIGHT = BASE_HEIGHT * SCREEN_MULITPLIER;
+const int SCREEN_WIDTH = BASE_WIDTH * SCREEN_MULITPLIER;
 
 int main()
 {
@@ -25,6 +34,41 @@ int main()
     // chip8.drawScreen();
 
     chip8.cycleCPU();
+
+    // Window initialization
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Window creation
+    window = SDL_CreateWindow("CHIP-8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    screenSurface = SDL_GetWindowSurface(window);   // Get window surface
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    SDL_Event e;
+    bool quit = false;
+    while (quit == false)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+    }
 
     return 0;
 }
