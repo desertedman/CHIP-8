4b87b734588b2e715979cba54051e41e3ff1beba
desertedman <jonathanjho03@gmail.com>
2025-04-01 02:32:51 -0700

    Fixed some opcodes and added comments

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -197,6 +197,7 @@ void CPU::executeOpcode(GPU &gpu, std::array<uint8_t, MEMORY> &memory)
 
             // If VX + VY > 0xFF (max value for unsigned 8bit), then set carry flag
             // Adjust equation to: VX > 0xFF - VY
+            // if (V[nibbles.sec >> 8] - V[nibbles.third >> 4] > (0xFF))
             if (V[nibbles.sec >> 8] > (0xFF - V[nibbles.third >> 4]))
             {
                 V[0xF] = 1; // Set carry flag
@@ -215,15 +216,14 @@ void CPU::executeOpcode(GPU &gpu, std::array<uint8_t, MEMORY> &memory)
             // Perform math operation first, then carry
             V[nibbles.sec >> 8] -= V[nibbles.third >> 4];
 
-            // If VX > VY, VF = 1; if VY < VX, VF = 0
-            // Think about it like this: VF is set to 1 prior to operation.
-            // If VX > VY, then no need to "borrow" from VF, leaving VF at 1.
-            // If VX < VY, then we borrow from VF, setting VF t0 0.
-            if (V[nibbles.sec >> 8] > V[nibbles.third >> 4])
+            // The comparison sign is negated - dont know why!
+            // If VX > VY, VF = 1; no borrow
+            if (V[nibbles.sec >> 8] <= V[nibbles.third >> 4])
             {
                 V[0xF] = 1;
             }
 
+            // VX < VY; borrow occurred
             else
             {
                 V[0xF] = 0;
@@ -237,8 +237,10 @@ void CPU::executeOpcode(GPU &gpu, std::array<uint8_t, MEMORY> &memory)
             // Modern behavior; VX = VY
             V[nibbles.sec >> 8] = V[nibbles.third >> 4];
 
-            V[0xF] = V[nibbles.sec >> 8] & 0b00000001; // Store last bit into register
-            V[nibbles.sec >> 8] >>= 1;                 // Bitshift right in place
+            uint8_t carryBit = V[nibbles.sec >> 8] & 0b00000001; // Store last bit into temp variable
+            V[nibbles.sec >> 8] >>= 1;                           // Bitshift right in place
+
+            V[0xF] = carryBit; // Set register to last bit
 
             break;
         }
@@ -270,8 +272,10 @@ void CPU::executeOpcode(GPU &gpu, std::array<uint8_t, MEMORY> &memory)
             // Modern behavior; VX = VY
             V[nibbles.sec >> 8] = V[nibbles.third >> 4];
 
-            V[0xF] = V[nibbles.sec >> 8] >> 7; // Store first bit into register
-            V[nibbles.sec >> 8] <<= 1;         // Bitshift left in place
+            uint8_t carryBit = V[nibbles.sec >> 8] >> 7; // Store first bit into temp variable
+            V[nibbles.sec >> 8] <<= 1;                   // Bitshift left in place
+
+            V[0xF] = carryBit; // Set register to first bit
 
             break;
         }
